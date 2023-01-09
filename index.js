11a7e82b2a3fd3440e376b7845d878c013e47b6b
the_aceix <aceixsmartX@gmail.com>
2023-01-09 18:29:50 +0000

    setup env configs, refactor routes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+import { setupRoutes } from './src/routes.js';
+import express from 'express';
+import dotenv from 'dotenv'
+
+const isProd = process.env.NODE_ENV === 'production'
+const ENV_FILEPATH = './.env'
+const LOCAL_ENV_FILEPATH = './.env.local'
+dotenv.config({ path: isProd ? ENV_FILEPATH : LOCAL_ENV_FILEPATH })
+
 const app = express();
 
-app.get ('/' , (req, res) =>{
-    res.send('Start')
-});
+setupRoutes(app)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.SERVER_PORT || 5000
 app.listen (PORT, () => console.log(`Listening on port ${PORT}`))
