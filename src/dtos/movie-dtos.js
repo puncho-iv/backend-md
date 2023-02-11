de59a52fb32912a3363f841dc5c1397783fc0dbf
Ebz <unicusdnl@gmail.com>
2023-02-11 03:15:59 +0000

    backend

--- src/dtos/movie-dtos.js
+++ src/dtos/movie-dtos.js
@@ -1,1 +1,7 @@
 // will contain request and response DTOs for movie CRUD
+class CreateMovieDto{
+    const (title, year, rating, genre, description) {
+        this.title=title;
+
+    }
+}
--- src/repositories/movie-repository.js
+++ src/repositories/movie-repository.js
@@ -1,7 +1,28 @@
 // will contain interfaces/classes/abstractions or implementations for DB methods
-export function getTopRatedMovies() {
+export function getTopRatedMovies(repository) {
    return repository.getMovies({
     sort:("DESC", "rating"),
     filter:("first 20 movies")
 });
 }
+
+export function getStreamingMovies(repository) {
+    return repository.getMovies({
+     sort:("DESC", "rating"),
+     filter:("first 20 movies")
+ });
+ }
+
+ export function getMoviesForRent(repository) {
+    return repository.getMovies({
+     sort:("DESC", "rating"),
+     filter:("first 20 movies")
+ });
+ }
+
+ export function getInTheatres(repository) {
+    return repository.getMovies({
+     sort:("DESC", "rating"),
+     filter:("first 20 movies")
+ });
+ }
